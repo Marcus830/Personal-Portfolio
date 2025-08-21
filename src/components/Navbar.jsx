9f19ac2d4dabb0dddb1e9a32a3e1fddc0b3e86fc
Marcus Michael <marcusmichealdb@gmail.com>
2025-08-20 21:16:33 -0500

    organize files and folders

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+function Navbar(){
+    return (
+        <nav>
+            <ul className="list-none m-0 p-0 overflow-hidden bg-gray-900">
+                <li className="inline float-left">
+                <a href="default.asp">Introduction</a>
+                </li>
+                <li className="inline float-left">
+                <a href="news.asp">About me</a>
+                </li>
+                <li className="inline float-left">
+                <a href="contact.asp">Contact</a>
+                </li>
+                <li className="inline float-left">
+                <a href="about.asp">Contact Me</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
